338dddaab85a723ef35ceb4ad127b7a65fd37342
Kristina H <42981533+kristinahocevar@users.noreply.github.com>
2019-05-31 12:59:33 +0200

    Add files via upload

---
+++ build/precache-manifest.d0900a07d28883c3a1be98cc2f3dfad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dde045b6c96c0cc7614e27fa50e384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbbe3947301283727f09",
+    "url": "/static/css/main.a7824949.chunk.css"
+  },
+  {
+    "revision": "4a2140eb8e5062542c2c",
+    "url": "/static/js/2.e617be74.chunk.js"
+  },
+  {
+    "revision": "fbbe3947301283727f09",
+    "url": "/static/js/main.e6bda56f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0a9bedec4f174a92f519b605c52db4d",
+    "url": "/static/media/ballpen.b0a9bede.jpg"
+  }
+]);
